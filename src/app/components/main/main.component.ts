09a1fa019cffd2d4808447a9ceb242c61fa77aca
kopaland <kopaland.devops@gmail.com>
2020-04-21 11:46:24 +0200

    update for heroku deploy

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,65 +1,70 @@
-import { Component, OnInit, ViewEncapsulation, ViewChild, ElementRef } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { AuthService } from '../../services/auth/auth.service';
-import { ChangeDetectorRef } from '@angular/core';
-import { MediaMatcher } from '@angular/cdk/layout';
-import { VersionService } from '../../services/tools/version.service';
-import { NavItem } from '../common/navigation/nav-item';
-import { NavService } from '../common/navigation/nav.service';
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation,
+  ViewChild,
+  ElementRef,
+} from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { AuthService } from "../../services/auth/auth.service";
+import { ChangeDetectorRef } from "@angular/core";
+import { MediaMatcher } from "@angular/cdk/layout";
+import { VersionService } from "../../services/tools/version.service";
+import { NavItem } from "../common/navigation/nav-item";
+import { NavService } from "../common/navigation/nav.service";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class MainComponent implements OnInit {
-
-  @ViewChild('appDrawer') appDrawer: ElementRef;
+  @ViewChild("appDrawer") appDrawer: ElementRef;
 
   public visible: boolean;
   public mobileQuery: MediaQueryList;
-  public version = '1.0.0';
+  public version = "1.0.0";
   public date = new Date();
-  public platform = 'Pack';
+  public platform = "Pack";
 
   /**
    * Custom slidebar
    */
   navItems: NavItem[] = [
     {
-      displayName: 'DDC',
-      iconName: 'folder_shared',
-      route: 'ddc',
+      displayName: "DDC",
+      iconName: "folder_shared",
+      route: "ddc",
       children: [
         {
-          displayName: 'Consulter',
-          iconName: 'visibility',
-          route: 'ddc/1'
+          displayName: "Consulter",
+          iconName: "visibility",
+          route: "ddc/1",
         },
         {
-          displayName: 'Editer',
-          iconName: 'create',
-          route: 'ddc/new'
-        }
-      ]
+          displayName: "Editer",
+          iconName: "create",
+          route: "ddc/new",
+        },
+      ],
     },
     {
-      displayName: 'MC',
-      iconName: 'blur_on',
-      route: 'mc',
+      displayName: "MC",
+      iconName: "blur_on",
+      route: "mc",
       children: [
         {
-          displayName: 'Consulter',
-          iconName: 'visibility',
-          route: 'mc/1'
+          displayName: "Consulter",
+          iconName: "visibility",
+          route: "mc/1",
         },
         {
-          displayName: 'Editer',
-          iconName: 'create',
-          route: 'mc/new'
-        }
-      ]
+          displayName: "Editer",
+          iconName: "create",
+          route: "mc/new",
+        },
+      ],
     },
   ];
 
@@ -74,10 +79,10 @@ export class MainComponent implements OnInit {
     media: MediaMatcher,
     private navService: NavService
   ) {
-    this.mobileQuery = media.matchMedia('(max-width: 800px)');
+    this.mobileQuery = media.matchMedia("(max-width: 800px)");
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
-    versionService.get().subscribe(data => {
+    versionService.get().subscribe((data) => {
       this.version = data.version;
       this.date = data.installationDate;
       this.platform = data.platform;
@@ -86,8 +91,8 @@ export class MainComponent implements OnInit {
 
   ngOnInit() {
     if (
-      localStorage.getItem('authenticatedRole') &&
-      localStorage.getItem('authenticatedRole') !== 'null'
+      localStorage.getItem("authenticatedRole") &&
+      localStorage.getItem("authenticatedRole") !== "null"
     ) {
       this.visible = true;
     } else {
